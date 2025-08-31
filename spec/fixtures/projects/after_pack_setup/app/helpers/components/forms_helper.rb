4d3fc7187255b2372f3018ca7b045cf2e50a2445
Aron Wolf <aronwolf90@gmail.com>
2025-08-31 04:03:49 +0200

    remove forgotten to remove files

--- spec/fixtures/projects/after_pack_setup/app/helpers/components/forms_helper.rb
+++
@@ -1,9 +0,0 @@
-module Components::FormsHelper
-  def render_form_with(**opts, &block)
-    form_with(**opts, builder: Shadcn::FormBuilder, &block)
-  end
-
-  def render_form_for(obj, **opts, &block)
-    form_for(obj, **opts, builder: Shadcn::FormBuilder, html: opts, &block)
-  end
-end
