cfea3249522018418ac4b99a9ff8ff6d944877f4
Klaus Weidinger <weidkl@gmx.de>
2025-09-12 19:15:46 +0200

    Simplify around hook

--- spec/support/rspec/config/fixtures.rb
+++ spec/support/rspec/config/fixtures.rb
@@ -5,12 +5,8 @@ RSpec.configure do |config|
 
   config.around do |example|
     Dir.mktmpdir do |tmp_dir|
-      example.metadata[:tmp_dir] = Pathname.new(tmp_dir)
+      @tmp_dir = Pathname.new(tmp_dir)
       example.run
     end
-  end
-
-  config.before do |example|
-    @tmp_dir = example.metadata[:tmp_dir]
   end
 end
