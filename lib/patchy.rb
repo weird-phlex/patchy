91e5b91a1b8026705e5d0ea39a9a11779e720f0d
Klaus Weidinger <weidkl@gmx.de>
2025-10-24 13:26:34 +0200

    Cleanup

--- lib/patchy.rb
+++ lib/patchy.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-if RUBY_VERSION < '3.2'
-  require 'polyfill-data'
-end
-
 require "zeitwerk"
 loader = Zeitwerk::Loader.for_gem
 loader.inflector.inflect('cli' => 'CLI')
